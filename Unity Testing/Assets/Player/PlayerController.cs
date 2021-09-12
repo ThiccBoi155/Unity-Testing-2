30f41d0cfaeb161af5fe118c49db8a9b7331015f
ThiccBoi155 <danielfich2@gmail.com>
2021-09-12 17:34:26 +0200

    Input system added

--- Unity Testing/Assets/Player/PlayerController.cs
+++ Unity Testing/Assets/Player/PlayerController.cs
@@ -1,10 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.InputSystem;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     //[Header("Input Settings")]
     //public PlayerInput playerInput;
+
+    public void OnJump(InputAction.CallbackContext value)
+    {
+        if (value.started)
+        {
+            Debug.Log("Jump");
+        }
+    }
+
+    public void OnMovement(InputAction.CallbackContext value)
+    {
+        Vector2 inputMovement = value.ReadValue<Vector2>();
+
+        Debug.Log(inputMovement);
+    }
 }

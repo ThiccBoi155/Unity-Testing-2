982770c63184966d7343af2a4e3488e331736899
ThiccBoi155 <danielfich2@gmail.com>
2021-09-22 15:21:05 +0200

    First working camera follow

--- Unity Testing/Assets/Camera/CameraBehavior.cs
+++ Unity Testing/Assets/Camera/CameraBehavior.cs
@@ -4,30 +4,88 @@ using UnityEngine;
 
 public class CameraBehavior : MonoBehaviour
 {
+    #region Debug
+    /*/
+    [Header("Debug values")]
+    public bool rotateX;
+    public bool rotateY;
+    public Vector3 customRotation;
+    public bool applyCustomRotation;
+    public bool resetRotation;
+
+    void DebugFunctions()
+    {
+        if (applyCustomRotation)
+        {
+            transform.Rotate(customRotation, Space.World);
+            applyCustomRotation = false;
+        }
+
+        if (resetRotation)
+        {
+            transform.rotation = new Quaternion();
+            resetRotation = false;
+        }
+    }
+    //*/
+    #endregion
+
+    [Header("Other debug values")]
+    public bool followPlayer;
+    public bool enableMeshRenderer;
+    public MeshRenderer meshRenderer;
+
     [Header("Refrences")]
     public Transform player;
     public Transform followCamera;
 
-    [Header("Settings")]
+    [Header("Continuous Settings")]
     public float followSpeed;
     public float rotationSpeed;
+    public float maxRotationX = 89;
+    public float minRotationX = -70;
+
+    public float cameraDistance = 8;
 
     private Vector2 gamepadLook;
+    private Vector3 eulerAngleRotation;
+
+    private void Start()
+    {
+        CameraLookAtOrigin();
+    }
 
     void FixedUpdate()
     {
-        Debug.Log(gamepadLook);
+        //DebugFunctions();
 
-        FollowPlayer();
+        meshRenderer.enabled = enableMeshRenderer;
+
+        if (followPlayer)
+            FollowPlayer();
         RotateCamera();
-        CameraLookAtOrigin();
+        SetCameraDistance();
     }
 
     void RotateCamera()
     {
-        transform.Rotate(new Vector3(gamepadLook.y, -gamepadLook.x, 0));
-    }
+        // Set eulerAngleRotation
+        eulerAngleRotation.x += -gamepadLook.y * rotationSpeed * Time.fixedDeltaTime;
+        eulerAngleRotation.y += gamepadLook.x * rotationSpeed * Time.fixedDeltaTime;
 
+        if (eulerAngleRotation.x < minRotationX)
+            eulerAngleRotation.x = minRotationX;
+
+        if (maxRotationX < eulerAngleRotation.x)
+            eulerAngleRotation.x = maxRotationX;
+
+        // Reset rotation
+        transform.rotation = new Quaternion();
+
+        // Set rotation to eulerAngleRotation
+        transform.Rotate(eulerAngleRotation, Space.World);
+    }
+    
     void CameraLookAtOrigin()
     {
         followCamera.LookAt(transform);
@@ -36,6 +94,11 @@ public class CameraBehavior : MonoBehaviour
     void FollowPlayer()
     {
         transform.position = Vector3.Lerp(transform.position, player.position, followSpeed * Time.fixedDeltaTime);
+    }
+    //////////////////////////////////////////////
+    void SetCameraDistance()
+    {
+        followCamera.localPosition = new Vector3(0, 0, -cameraDistance);
     }
     //////////////////////////////////////////////
     public void UpdateCameraLook(Vector2 look)
